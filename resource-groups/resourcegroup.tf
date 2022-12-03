a67891a8c19d24973844fafe14faf7abc8b713de
Deivids Egle <deivids.egle@gmail.com>
2022-12-03 17:30:55 +0000

    added featuers block

--- resource-groups/resourcegroup.tf
+++ resource-groups/resourcegroup.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+        sosource = "hashicorp/azurerm"
+        versversion = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "terra-demo-02" {
   name = "rg-dev-tf-001"
   location = "var.location"
