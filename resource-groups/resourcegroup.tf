0bd74d8684bff461524539fe929e35f0548f11c6
Deivids Egle <deivids.egle@gmail.com>
2022-12-03 19:52:42 +0000

    Updated .gitignore

--- resource-groups/resourcegroup.tf
+++ resource-groups/resourcegroup.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
         source = "hashicorp/azurerm"
-        version = "=3.0.0"
+        version = "=3.34.0"
     }
   }
 }
@@ -13,7 +13,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "terra-demo-02" {
   name = "rg-dev-tf-001"
-  location = "var.location"
+  location = "uksouth"
   tags = {
     CreatedBy = "Deivids",
     CostCenter = "IT Training"
