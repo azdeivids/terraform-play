8bcb002d2343836e92419490f2027e975b86fd7c
Deivids Egle <deivids.egle@gmail.com>
2022-12-06 21:38:07 +0000

    Specified string variables

--- lab02/variables.tf
+++ lab02/variables.tf
@@ -1,12 +1,15 @@
 variable "rgname01" {
   description = "Name of the resource group where all the resources will be placed."
+  type = string
   default = "tf-websrv-rg-001"
 }
 variable "appsvcname" {
   description = "Name of the app service plan."
+  type = string
   default = "appsvc-plan-001"
 }
 variable "webappname" {
   description = "Name of the web app."
+  type = string
   default = "tfapp001"
 }
