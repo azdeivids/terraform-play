b99cd54ac9ced30f603ae923efe240995d433e3a
Deivids Egle <deivids.egle@gmail.com>
2022-12-06 21:00:03 +0000

    Fixed resources

--- lab02/resource.tf
+++ lab02/resource.tf
@@ -21,6 +21,6 @@ resource "azurerm_app_service_source_control" "sourcecontorl" {
   app_id = azurerm_linux_web_app.webapp.id
   repo_url = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
   branch = "master"
-  use_manual_integration = 
+  use_manual_integration = false
   use_mercurial = false
 }
