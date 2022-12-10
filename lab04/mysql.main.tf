8ac55de6b9893f43d3429e6fa9ea5783b424e9f5
Deivids Egle <deivids.egle@gmail.com>
2022-12-10 23:04:53 +0000

    fixed values

--- lab04/mysql.main.tf
+++ lab04/mysql.main.tf
@@ -16,23 +16,23 @@ terraform {
     }
 
 }
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "tfrg04" {
+  name     = "tf-mysql-rg"
+  location = "UK South"
 }
-resource "azurerm_mysql_flexible_server" "example" {
+resource "azurerm_mysql_flexible_server" "tfsrv04" {
   name                   = "example-mysql-flexible-server-azdeivids1016"
-  resource_group_name    = azurerm_resource_group.example.name
-  location               = azurerm_resource_group.example.location
+  resource_group_name    = azurerm_resource_group.tfrg04.name
+  location               = azurerm_resource_group.tfrg04.location
   administrator_login    = var.db_username
   administrator_password = var.db_password
   sku_name               = "B_Standard_B1s"
 }
 
-resource "azurerm_mysql_flexible_database" "example" {
+resource "azurerm_mysql_flexible_database" "mysqldb04" {
   name                = "exampledb"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_mysql_flexible_server.example.name
+  resource_group_name = azurerm_resource_group.tfrg04.name
+  server_name         = azurerm_mysql_flexible_server.tfsrv04.name
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
