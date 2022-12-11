46bfd6ba847d13b640951760d1757d6814d8adbc
Deivids Egle <deivids.egle@gmail.com>
2022-12-11 23:14:51 +0000

    Removed provider from the module

--- lab05/modules/services/vmss/main.tf
+++ lab05/modules/services/vmss/main.tf
@@ -1,22 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.34.0"
-    }
-  }
-    backend "azurerm" {
-        resource_group_name  = "terraform-state-storage-uksouth"
-        storage_account_name = "tfstate428150812"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-
-}
-
 resource "azurerm_resource_group" "tfrg05" {
   name     = "${var.prefix}-resources"
   location = var.location
