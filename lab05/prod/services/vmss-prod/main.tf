a9fe46845982e2dec4ce97a6d0402798d2e05d86
Deivids Egle <deivids.egle@gmail.com>
2022-12-12 21:43:15 +0000

    Added module to the main.tf

--- lab05/prod/services/vmss-prod/main.tf
+++ lab05/prod/services/vmss-prod/main.tf
@@ -9,3 +9,11 @@ terraform {
     }
   }
 }
+module "vmss" {
+    source = "/home/deivids/terraform-play/lab05/modules/services/vmss"
+    
+    remote_state_rg = var.remote_state_rg
+    remote_state_st_acc = var.remote_state_st_acc
+    remote_state_container = var.remote_state_container
+    remote_state_key = var.remote_state_key
+}
