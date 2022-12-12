a0d6adf044d85073c315d5b0c078dbe6db2cd5a4
Deivids Egle <deivids.egle@gmail.com>
2022-12-12 13:06:19 +0000

    ...

--- lab05/stage/services/vmss/vmss.tf
+++ lab05/stage/services/vmss/vmss.tf
@@ -8,13 +8,9 @@ terraform {
       version = "=3.34.0"
     }
   }
-    backend "azurerm" {
-        resource_group_name  = "terraform-state-storage-uksouth"
-        storage_account_name = "tfstate428150812"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
 }
 module "vmss05" {
   source = "/home/deivids/terraform-play/lab05/modules/services/vmss"
+  storage_account_name = "tfstate428150812"
+  container_name = "tfstate02"
 }
