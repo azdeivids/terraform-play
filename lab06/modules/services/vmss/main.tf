fa2e4e86e7aba1a8370c48f17e2c3cffdf7c1826
Deivids Egle <deivids.egle@gmail.com>
2022-12-13 17:20:39 +0000

    Added scaling conditions

--- lab06/modules/services/vmss/main.tf
+++ lab06/modules/services/vmss/main.tf
@@ -114,6 +114,76 @@ resource "azurerm_linux_virtual_machine_scale_set" "lin-vmss-06" {
   # As noted in Terraform documentation https://www.terraform.io/docs/providers/azurerm/r/linux_virtual_machine_scale_set.html#load_balancer_backend_address_pool_ids
   depends_on = [azurerm_lb_rule.lb_rule06]
 }
+resource "azurerm_monitor_autoscale_setting" "scale06" {
+  name                = "myAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.rg06.name
+  location            = azurerm_resource_group.rg06.location
+  target_resource_id  = azurerm_linux_virtual_machine_scale_set.lin-vmss-06.id
+
+  profile {
+    name = "defaultProfile"
+
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 5
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.lin-vmss-06.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 85
+        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
+        dimensions {
+          name     = "AppName"
+          operator = "Equals"
+          values   = ["App1"]
+        }
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "Percentage CPU"
+        metric_resource_id = azurerm_linux_virtual_machine_scale_set.lin-vmss-06.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 25
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }
+
+  notification {
+    email {
+      send_to_subscription_administrator    = true
+      send_to_subscription_co_administrator = false
+      custom_emails                         = ["deivids@deividsegle.com"]
+    }
+  }
+}
 data "terraform_remote_state" "tfstate05" {
   backend = "azurerm"
   config = {
