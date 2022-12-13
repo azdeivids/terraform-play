27bedffc08bf481617e318eb5511c9bd6652128b
Deivids Egle <deivids.egle@gmail.com>
2022-12-13 17:57:21 +0000

    Fixed variables

--- lab06/stage/services/vmss-stage/variables.tf
+++ lab06/stage/services/vmss-stage/variables.tf
@@ -1,26 +1,26 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
   type        = string
-  default = "tf-lab06"
+  default     = "tf-lab06"
 }
 
 variable "remote_state_rg" {
   description = "RG where the remote state st account is being stored"
-  type = string
-  default = "terraform-state-storage-uksouth"
+  type        = string
+  default     = "terraform-state-storage-uksouth"
 }
 variable "remote_state_st_acc" {
   description = "Container blob where the remote state will be stored"
   type        = string
-  default = "tfstate428150812"
+  default     = "tfstate428150812"
 }
 variable "remote_state_key" {
   description = "Path to the storage account remote state key"
   type        = string
-  default = "terraform.tfstate"
+  default     = "terraform.tfstate"
 }
 variable "tfstate_container" {
   description = "Name of the container where the tf state file is to be stored"
   type        = string
-  default = "tfstate"
+  default     = "tfstate"
 }
