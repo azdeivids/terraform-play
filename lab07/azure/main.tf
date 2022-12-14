29c2db5f9f082f45f99a17e05525fce90c987410
Deivids Egle <deivids.egle@gmail.com>
2022-12-14 15:12:52 +0000

    Fixed provider version

--- lab07/azure/main.tf
+++ lab07/azure/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
         source = [hashicorp/azurerm]
-        verversion = "~> 3.0.0 "        
+        version = "~> 3.0.0 "        
     }
   }
 }
