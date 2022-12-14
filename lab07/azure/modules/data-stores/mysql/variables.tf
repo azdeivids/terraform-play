675c0d5fab824a7dcc667538db765f2eb0c54a95
Deivids Egle <deivids.egle@gmail.com>
2022-12-14 18:34:44 +0000

    Added pdnsz var

--- lab07/azure/modules/data-stores/mysql/variables.tf
+++ lab07/azure/modules/data-stores/mysql/variables.tf
@@ -1,7 +1,7 @@
 # remote state variables
 variable "remote_state_rg" {
   description = "RG where the remote state st account is being stored"
-  type = string  
+  type        = string
 }
 variable "remote_state_st_acc" {
   description = "Container blob where the remote state will be stored"
@@ -30,26 +30,34 @@ variable "prod_subscription" {
 # resource variables
 variable "rg_prefix" {
   description = "Resource group prefix to be used during deployment"
-  type = string
+  type        = string
 }
 variable "location" {
   description = "Location where resources are to be created"
-  type = string
+  type        = string
+}
+variable "pdnsz" {
+  description = "Deploy private dns zone"
+  type        = bool
 }
 # set TF_VAR_db_username=<username>
 variable "db_username" {
   description = "MySQL DB Username"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 # set TF_VAR_db_password=<password>
 variable "db_password" {
   description = "MySQL DB User password"
-  type = string
-  sensitive = true
+  type        = string
+  sensitive   = true
 }
 variable "backup_retention_days" {
   description = "Backup retention days for the mysql flexible server"
-  type = number
-  default = 7
+  type        = number
+  default     = 7
+}
+variable "geo_redundant_backup_enabled" {
+  description = "Enable/disable GEO redundancy"
+  type        = bool
 }
