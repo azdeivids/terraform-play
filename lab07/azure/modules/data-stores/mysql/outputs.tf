fea8bbc154f0611b0cec63afb731c0a31b119f19
Deivids Egle <deivids.egle@gmail.com>
2022-12-14 19:25:17 +0000

    Added outputs for mysql flex server id and fqdn

--- lab07/azure/modules/data-stores/mysql/outputs.tf
+++ lab07/azure/modules/data-stores/mysql/outputs.tf
@@ -6,3 +6,9 @@ output "prod_subscription" {
   description = "Subscription Id stored in dev Subscription"
   value       = data.azurerm_subscription.prod_subscription.id
 }
+output "id" {
+  value = azurerm_mysql_flexible_server.mysqlsrv07.id
+}
+output "fqdn" {
+  value = azurerm_mysql_flexible_server.mysqlsrv07.fqdn
+}
