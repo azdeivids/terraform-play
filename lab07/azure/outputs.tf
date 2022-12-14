42a07cfcf0c32fa1b6ca3d10a9f483024080bebd
Deivids Egle <deivids.egle@gmail.com>
2022-12-14 18:10:21 +0000

    Moved files

--- lab07/azure/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "dev_subscription" {
-  description = "Subscription Id stored in dev Subscription"
-  value       = data.azurerm_subscription.dev_subscription.id
-}
-output "prod_subscription" {
-  description = "Subscription Id stored in dev Subscription"
-  value       = data.azurerm_subscription.prod_subscription.id
-}
