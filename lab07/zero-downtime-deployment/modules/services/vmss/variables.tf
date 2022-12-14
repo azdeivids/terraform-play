b5bcb2593f2b87c617e56ba57c12c38eb7c9aaac
Deivids Egle <deivids.egle@gmail.com>
2022-12-14 10:39:52 +0000

    Added variables

---
+++ lab07/zero-downtime-deployment/modules/services/vmss/variables.tf
@@ -0,0 +1,43 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  type        = string
+  default     = "UK South"
+}
+# variables to specify the remote state
+variable "remote_state_rg" {
+  description = "RG where the remote state st account is being stored"
+  type        = string
+}
+variable "remote_state_st_acc" {
+  description = "Container blob where the remote state will be stored"
+  type        = string
+}
+variable "remote_state_key" {
+  description = "Path to the storage account remote state key"
+  type        = string
+}
+variable "tfstate_container" {
+  description = "Name of the container where the tf state file is to be stored"
+  type        = string
+}
+variable "enable_autoscaling" {
+  description = "Enable or disable autoscaling using"
+  type        = bool
+}
+variable "default_size" {
+  description = "Default vmss instance size"
+  type        = number
+}
+variable "min_size" {
+  description = "Minium vmss instance size"
+  type        = number
+}
+variable "max_size" {
+  description = "Maximum vmss instance size"
+  type        = number
+}
